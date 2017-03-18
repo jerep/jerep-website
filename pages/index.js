8feb1310aba9fec1f17cce81addee7756b0b3c27
Jere Piispanen <jere.piispanen@gmail.com>
2017-03-18 19:59:31 +0200

    Improve responsiveness

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import SomeIcons from '../components/SomeIcons'
 export default () => (
   <div className={cx.root}>
     <div className={cx.card}>
-      <div className={cx.left}>
+      <div className={cx.side}>
         <img className={cx.avatar} src="/static/avatar.jpg" />
       </div>
       <div className={cx.body}>
@@ -28,14 +28,19 @@ const cx = {
     justifyContent: 'center',
     height: '100vh',
     color: '#000',
-    fontSize: '16px',
+    fontSize: '16px'
   }),
   card: cxs({
     display: 'flex',
     alignItems: 'flex-start',
+    margin: '10px',
+    '@media (max-width: 640px)': {
+      flexDirection: 'column',
+      textAlign: 'center'
+    },
     h1: {
       fontSize: '16px',
-      marginTop: 0
+      margin: '0 0 18px 0'
     },
     a: {
       color: '#000',
@@ -44,11 +49,20 @@ const cx = {
       ':hover': {
         opacity: 0.6
       }
+    },
+    p: {
+      margin: '0 0 18px 0'
     }
   }),
-  left: cxs({
+  side: cxs({
     display: 'flex',
-    marginRight: '40px'
+    marginRight: '40px',
+    '@media (max-width: 640px)': {
+      width: '100%',
+      justifyContent: 'center',
+      marginRight: 0,
+      marginBottom: '35px'
+    }
   }),
   avatar: cxs({
     width: '128px',
