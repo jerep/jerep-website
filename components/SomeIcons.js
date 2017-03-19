9dab9d668e893f973420c07fe856240b81a4dbab
Jere Piispanen <jere.piispanen@gmail.com>
2017-03-19 12:39:31 +0200

    Fix some icons margin

--- components/SomeIcons.js
+++ components/SomeIcons.js
@@ -1,15 +1,15 @@
 import cxs from 'cxs'
 
 export default () => (
-  <div>
+  <div className={cx}>
     <a href="mailto:jere.piispanen@gmail.com" target="_blank">
-      <i className={['fa fa-2x fa-envelope-square', cx.margin].join(' ')} />
+      <i className="fa fa-2x fa-envelope-square" />
     </a>
     <a href="https://www.linkedin.com/in/jerep/" target="_blank">
-      <i className={['fa fa-2x fa-linkedin-square', cx.margin].join(' ')} />
+      <i className="fa fa-2x fa-linkedin-square" />
     </a>
     <a href="https://github.com/jerep" target="_blank">
-      <i className={['fa fa-2x fa-github-square', cx.margin].join(' ')} />
+      <i className="fa fa-2x fa-github-square" />
     </a>
     <a href="https://www.instagram.com/jerepx/" target="_blank">
       <i className="fa fa-2x fa-instagram" />
@@ -17,8 +17,8 @@ export default () => (
   </div>
 )
 
-const cx = {
-  margin: cxs({
+const cx = cxs({
+  a: {
     marginRight: '10px'
-  })
-}
+  }
+})
